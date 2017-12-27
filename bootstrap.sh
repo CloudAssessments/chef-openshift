af686eabb712db057c03b09fb293e458f9c45438
thewarhammer <joshualwarrick@yahoo.com>
2017-12-27 09:48:40 -0600

    added post run cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,4 @@ cp solo.rb openshift.json /root
 cd /root
 git clone https://github.com/chef-cookbooks/yum.git
 chef-solo -c solo.rb -j openshift.json
+find . ! -name 'anaconda-ks.cfg' -type f -exec rm -rf {} +
